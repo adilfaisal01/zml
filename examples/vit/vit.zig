5cb05a59d85234337298b7ff6186032c6a9e899d
Adil Faisal <adilfaisal01@proton.me>
2026-08-25 12:30:03 -0400

    feat: add ViT example with configuration structs and model scaffold

---
+++ examples/vit/vit.zig
@@ -0,0 +1,59 @@
+const std= @import("std");
+const zml= @import("zml");
+const stdx= zml.stdx;
+
+const log= std.log.scoped(.vit);
+
+// specifying the types of operations that can be done in a ViT
+pub const Pooling= enum{cls, mean, none};
+pub const NormType= enum{rms_norm, layer_norm};
+pub const PatchEmbedKind= enum{conv,hybrid_conv, linear};
+
+// ViT config based off Hugging face transformers repo
+pub const VitConfig= struct {
+patch_size:u32,
+image_size: stdx.json.Union(union(enum) {
+      int: u32,
+      ints:[]u32
+}),
+layer_norm_eps:f32=1e-6,
+input_num_channels:u32=3,
+num_attention_heads:u32,
+hidden_size: u32,
+mlp_size:u32,
+num_hidden_layers:u32,
+qkv_bias: bool=true,
+hidden_act: zml.nn.Activation= .gelu,
+pooler_act: zml.nn.Activation =.tanh,
+pooler_output_size: u32,
+
+// getting image size as a 2d dimension
+
+pub fn imageWh(self: VitConfig) [2]u32 {
+    return switch(self.image_size.value){
+        .int => |v| .{v,v},
+        .ints => |s| .{s[0], s[1]}
+    };
+}
+
+// getting patches as a 2D 
+
+pub fn patchWH(self: VitConfig) [2]u32
+{
+    return .{self.patch_size, self.patch_size};
+}
+
+// getting the number of patches created from the image and patch size configuraations
+pub fn numPatches(self: VitConfig) u32 {
+    const image_size:[2]u32=self.imageWh();
+    const patch_size:[2]u32= self.patchWH();
+
+    return (image_size[0]/patch_size[0])*(image_size[1]*patch_size[1]);
+}
+};
+
+pub const buffer= zml.Bufferized(Model);
+
+pub const Model = struct{
+    embeddings: Embe
+};
